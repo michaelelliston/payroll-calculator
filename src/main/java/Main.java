7d17630d398d7ff9cfb749734a63a0017da13fcd
Michael <michael.elliston95@gmail.com>
2025-09-25 11:05:06 -0500

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = myScanner.nextLine();
+
+        System.out.println("Enter the number of hours you've worked:");
+        String inputOne = myScanner.nextLine();
+        float hoursWorked = Float.parseFloat(inputOne);
+
+        System.out.println("Enter your pay rate:");
+        String inputTwo = myScanner.nextLine();
+        float payRate = Float.parseFloat(inputTwo);
+
+        float grossPay = hoursWorked * payRate;
+        if (hoursWorked > 40) {
+            int overtimeHours = (int)hoursWorked - 40;
+            float overtimePay = (float) (overtimeHours * payRate * 1.5);
+            float finalPay = 40 * payRate + overtimePay;
+            System.out.printf("Hello, %s. Your gross pay with overtime is $%.2f.", name, finalPay);
+        }
+        else System.out.printf("Hello, %s. Your gross pay is $%.2f.", name, grossPay);
+    }
 }
